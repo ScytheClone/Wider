dfb2f8176afa7607870676f0c650f7d45186709f
Gustav <gustavc24@gmail.com>
2020-11-25 11:52:58 +0100

    Creat post fungerar

--- wider/models/Post.php
+++ wider/models/Post.php
@@ -16,7 +16,6 @@ Class Post{
     public $username;
     public $serviceTitle;
     public $serviceDate;
-    public $serviceID;
     public $serviceType;
 
     //Constructor with db
@@ -68,9 +67,7 @@ Class Post{
             postDate = CURDATE(),
             username = :username,
             pageID = :pageID,
-            imageURL = :imageURL,
-            serviceID = :serviceID';
-
+            imageURL = :imageURL';
             //Preparing statement
             $stmt = $this->conn->prepare($query);
 
@@ -80,7 +77,6 @@ Class Post{
             $this->username =htmlspecialchars(strip_tags($this->username));
             $this->pageID =htmlspecialchars(strip_tags($this->pageID));
             $this->imageURL =htmlspecialchars(strip_tags($this->imageURL));
-            $this->serviceID =htmlspecialchars(strip_tags($this->serviceID));
 
             //Bind data
             $stmt->bindParam(':postTitle', $this->postTitle);
@@ -88,7 +84,6 @@ Class Post{
             $stmt->bindParam(':username', $this->username);
             $stmt->bindParam(':pageID', $this->pageID);
             $stmt->bindParam(':imageURL', $this->imageURL);
-            $stmt->bindParam(':serviceID', $this->serviceID);
 
             //Executing query
             if($stmt->execute()){
@@ -178,7 +173,6 @@ Class Post_activate{
     public $username;
     public $serviceTitle;
     public $serviceDate;
-    public $serviceID;
     public $serviceType;
 
     //Constructor with db
@@ -251,7 +245,6 @@ Class Post_history{
     public $imageURL;
     public $postTitle;
     public $pageID;
-    public $serviceID;
     public $username;
     public $serviceType;
 
